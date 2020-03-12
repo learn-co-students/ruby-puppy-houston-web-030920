bfdf523cb38dbcc6b54914a1a6c67fa9cd8d9f11
faizah1992 <faizah1992@gmail.com>
2020-03-12 18:21:25 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
-# Add your code here
+class Dog 
+    
+    attr_reader :name
+    @@all =[]
+
+    def initialize(name)
+        @name = name
+        self.save
+    end
+
+    def Dog.all
+        return @@all
+    end
+
+    def Dog.clear_all
+        @@all.clear
+    end
+
+    def Dog.print_all
+        @@all.each do |dog|
+            puts dog.name
+        end
+    end
+
+    def save 
+        @@all << self
+    end 
+
+
+end
